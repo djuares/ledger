ed67b6a5ac76fe824acbef58652df2bc39d075f1
Damaris <damarismjuares@gmail.com>
2025-09-04 22:41:05 -0300

    Refactoring

--- lib/transactions.ex
+++ lib/transactions.ex
@@ -1,38 +1,40 @@
 defmodule Ledger.Transactions do
   def list(input_file, origin_account, output_file) do
-    case open_input_file(input_file, origin_account) do
-      content when is_binary(content) -> {:ok, content}
-      error_message -> {:error, error_message}
-      end
-    end
-
-   def open_input_file(input_file, "0") do
-  case File.read(input_file) do
-    {:ok, content} ->
-      content
-      |> String.split("\n")
-      |> Enum.filter(&(&1 != ""))
-      |> Enum.join("\n")
-
-    {:error, error} ->
-      "Error: #{:file.format_error(error)}"
-    end
-  end
-  def open_input_file(input_file, origin_account) do
-  case File.read(input_file) do
-    {:ok, content} ->
-      content
-      |> String.split("\n")
-      |> Enum.filter(&(&1 != ""))
-      |> Enum.filter(fn line ->
-        parts = String.split(line, ";")
-        Enum.at(parts, 5) == origin_account || Enum.at(parts, 6) == origin_account
-      end)
-      |> Enum.join("\n")
-
-    {:error, error} ->
-      "Error: #{:file.format_error(error)}"
+    with {:ok, content} <- File.read(input_file),
+         processed_content <- process_content(content, origin_account),
+         {:ok, result} <- write_output_file(output_file, processed_content) do
+      {:ok, result}
+    else
+      {:error, error} -> {:error, error}
     end
   end
 
+  defp write_output_file(output_file, content) do
+    case File.write(output_file, content) do
+      :ok ->
+        IO.puts("Archivo guardado: #{output_file}")
+        {:ok, content}
+
+      {:error, error} ->
+        {:error, "Error al escribir: #{:file.format_error(error)}"}
+    end
+  end
+
+  defp process_content(content, "0") do
+    content
+    |> String.split("\n")
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.join("\n")
+  end
+
+  defp process_content(content, origin_account) do
+    content
+    |> String.split("\n")
+    |> Enum.filter(&(&1 != ""))
+    |> Enum.filter(fn line ->
+      parts = String.split(line, ";")
+      Enum.at(parts, 5) == origin_account || Enum.at(parts, 6) == origin_account
+    end)
+    |> Enum.join("\n")
+  end
 end
